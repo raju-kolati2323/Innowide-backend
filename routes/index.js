4540c4a3db135801f2e3716c436277f4d94bd700
raju-kolati2323 <raju.kolati5@gmail.com>
2024-09-25 12:57:16 +0530

    Innowide

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,23 @@ router.post('/contact-us',(req,res)=>{
   .catch((err)=>res.status(404).send('Error submitting form:', err.message))
 })
 
+router.get('/contact-us',(req,res)=>{
+  contactUs.find({})
+  .then((pro)=>{res.status(200).send(pro)})
+  .catch((err)=>{res.status(404).send(err)})
+});
+
 router.post('/get-in-touch',(req,res)=>{
   var touch=new getInTouch(req.body);
   touch.save()
   .then((pro)=>res.status(200).send('Form submitted successfully'))
   .catch((err)=>res.status(404).send('Error submitting form:', err.message))
-})  
+}) ; 
+
+router.get('/get-in-touch',(req,res)=>{
+  getInTouch.find({})
+  .then((pro)=>{res.status(200).send(pro)})
+  .catch((err)=>{res.status(404).send(err)})
+});
 
 module.exports = router;
